4d14997d9e9e8849207da5cdb952d7847329b8db
Victor Oliveira <victor.oliveira20@icloud.com>
2023-04-30 13:12:32 -0300

    Criada classe PosicaoXadrez

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -6,21 +6,11 @@ namespace ChessGame
     {
         static void Main(string[] args)
         {
-            try { 
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 5));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+            Console.WriteLine(pos);
 
-            Tela.ImprimirTabuleiro(tab);
-
-            }
-
-            catch(TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos.toPosicao());
 
             Console.ReadLine();
         }
---
+++ ChessGame/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,25 @@
+﻿using ChessGame.tabuleiro;
+namespace ChessGame.xadrez
+{
+    internal class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}

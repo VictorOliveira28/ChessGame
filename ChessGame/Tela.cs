af83db9f6f454f1ed8afe3b2181d624dd9679555
Victor Oliveira <victor.oliveira20@icloud.com>
2023-04-30 13:24:42 -0300

    Melhorando a implementação do metodo imprimirTabuleiro

--- ChessGame/Tela.cs
+++ ChessGame/Tela.cs
@@ -8,6 +8,7 @@ namespace ChessGame
         {
             for(int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for(int j = 0; j < tab.Colunas; j++)
                 {
                     if(tab.peca(i, j) == null){
@@ -16,10 +17,26 @@ namespace ChessGame
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }                    
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+        public static void imprimirPeca(Peca peca)
+        {
+            if(peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
